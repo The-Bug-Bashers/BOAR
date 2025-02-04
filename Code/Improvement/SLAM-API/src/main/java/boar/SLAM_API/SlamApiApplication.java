4f27a7c5e0cf77ae690c84348a21e2abf51c4849
Flottegurke <justus.dicker@web.de>
2025-02-05 00:14:56 +0100

    create structure

---
+++ Code/Improvement/SLAM-API/src/main/java/boar/SLAM_API/SlamApiApplication.java
@@ -0,0 +1,13 @@
+package boar.SLAM_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlamApiApplication.class, args);
+	}
+
+}
